c5af3a899651cd2f7a98e29548646b0b37fb009d
Udit kumar <kumar2006udit@gmail.com>
2026-09-05 18:42:19 +0530

    Update basics.py

--- week_2/day_10_selenium/basics.py
+++ week_2/day_10_selenium/basics.py
@@ -8,13 +8,11 @@ with open("udit.txt","w",encoding="utf-8") as wf:
         driver.get(f"https://www.amazon.in/s?k={query}&page={i}&xpid=YQW1510Tgg95c&crid=1WH558AEHBCL0&qid=1744203164&sprefix=laptop%2Caps%2C302&ref=sr_pg_2")
         elems=driver.find_elements(By.CLASS_NAME, "puis-card-container")
         for elem in elems:
-            # print(elem.text) 
             name=elem.find_element(By.CSS_SELECTOR,"h2").text
             price=elem.find_element(By.CSS_SELECTOR,"span.a-price-whole").text
             element=elem.find_element(By.CSS_SELECTOR,"a")
             link=element.get_attribute('href')
             actual_link="https://www.amazon.com"+link
-
             wf.write(name+"-"+price+link)
             wf.write("\n")
             break
