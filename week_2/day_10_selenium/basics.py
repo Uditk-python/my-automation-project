f2a35078cde1d844a56b6149fdaea4b8fde7cb2f
Udit kumar <kumar2006udit@gmail.com>
2026-09-05 18:41:51 +0530

    Update basics.py

--- week_2/day_10_selenium/basics.py
+++ week_2/day_10_selenium/basics.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-# from selenium.webdriver.common.keys import keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 driver=webdriver.Chrome()
